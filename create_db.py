36090faec7350d79e0f61a1d9be1b01ef169d20c
kur <rhdnfka94@gmail.com>
2019-03-30 08:56:07 +0900

    Refactor global script to create_table func

--- create_db.py
+++ create_db.py
@@ -1,53 +1,55 @@
 import sqlite3
-db = sqlite3.connect('szmc.db')
-print(type(db))
+def create_table(name='szmc.db'):
+    with sqlite3.connect(name) as db:
+        db.execute('PRAGMA foreign_keys = ON;')
+        db.execute('''
+        CREATE TABLE IF NOT EXISTS data(
+            id      TEXT                                            PRIMARY KEY NOT NULL,
+            text    TEXT CHECK( text IN ('?','!','O','A','H','N'))  NOT NULL DEFAULT '?',
+            cnet    TEXT CHECK( cnet IN ('?','R','D','T')        )  NOT NULL DEFAULT '?',
+            snet    TEXT CHECK( snet IN ('?','R','D','T')        )  NOT NULL DEFAULT '?'
+        ); 
+        ''')
 
-db.execute('PRAGMA foreign_keys = ON;')
-db.execute('''
-CREATE TABLE data(
-    id      TEXT                                            PRIMARY KEY NOT NULL,
-    text    TEXT CHECK( text IN ('?','!','O','A','H','N'))  NOT NULL DEFAULT '?',
-    cnet    TEXT CHECK( cnet IN ('?','R','D','T')        )  NOT NULL DEFAULT '?',
-    snet    TEXT CHECK( snet IN ('?','R','D','T')        )  NOT NULL DEFAULT '?'
-); 
-''')
+        db.execute('''
+        CREATE TABLE IF NOT EXISTS image(
+            id          TEXT                                                PRIMARY KEY NOT NULL,
+            file_path   TEXT                                                NOT NULL,
+            extension   TEXT    CHECK( extension IN ('png','jpg','gif') )   NOT NULL,
+            file_size   INTEGER CHECK( file_size > 0                    )   NOT NULL,
+            height      INTEGER CHECK( height > 0                       )   NOT NULL,
+            width       INTEGER CHECK( width > 0                        )   NOT NULL,
+            FOREIGN KEY(id) REFERENCES data(id)
+        ); 
+        ''')
 
-db.execute('''
-CREATE TABLE image(
-    id          TEXT                                                PRIMARY KEY NOT NULL,
-    file_path   TEXT                                                NOT NULL,
-    extension   TEXT    CHECK( extension IN ('png','jpg','gif') )   NOT NULL,
-    file_size   INTEGER CHECK( file_size > 0                    )   NOT NULL,
-    height      INTEGER CHECK( height > 0                       )   NOT NULL,
-    width       INTEGER CHECK( width > 0                        )   NOT NULL,
-    FOREIGN KEY(id) REFERENCES data(id)
-); 
-''')
+        db.execute('''
+        CREATE TABLE IF NOT EXISTS metadata(
+            id          TEXT                                    PRIMARY KEY NOT NULL,
+            highres     INTEGER CHECK( highres    IN (0,1) )    NOT NULL DEFAULT 0,
+            absurdres   INTEGER CHECK( absurdres  IN (0,1) )    NOT NULL DEFAULT 0,
+            comic       INTEGER CHECK( comic      IN (0,1) )    NOT NULL DEFAULT 0,
+            monochrome  INTEGER CHECK( monochrome IN (0,1) )    NOT NULL DEFAULT 0,
+            greyscale   INTEGER CHECK( greyscale  IN (0,1) )    NOT NULL DEFAULT 0,
+            FOREIGN KEY(id) REFERENCES data(id)
+        ); 
+        ''')
 
-db.execute('''
-CREATE TABLE metadata(
-    id          TEXT                                    PRIMARY KEY NOT NULL,
-    highres     INTEGER CHECK( highres    IN (0,1) )    NOT NULL DEFAULT 0,
-    absurdres   INTEGER CHECK( absurdres  IN (0,1) )    NOT NULL DEFAULT 0,
-    comic       INTEGER CHECK( comic      IN (0,1) )    NOT NULL DEFAULT 0,
-    monochrome  INTEGER CHECK( monochrome IN (0,1) )    NOT NULL DEFAULT 0,
-    greyscale   INTEGER CHECK( greyscale  IN (0,1) )    NOT NULL DEFAULT 0,
-    FOREIGN KEY(id) REFERENCES data(id)
-); 
-''')
-#db.execute('INSERT INTO tab VALUES("a","ppap")')
-db.commit()
+        '''
+        cursor = db.cursor()
+        cursor.execute('SELECT * FROM tab')
+        print(cursor.fetchone())
+        print(cursor.fetchone())
+        print(cursor.fetchone())
+        print(cursor.fetchall())
 
-'''
-cursor = db.cursor()
-cursor.execute('SELECT * FROM tab')
-print(cursor.fetchone())
-print(cursor.fetchone())
-print(cursor.fetchone())
-print(cursor.fetchall())
+        cursor.execute('SELECT * FROM tab')
+        print(cursor.fetchall())
+        '''
+        db.commit()
+        db.close()
 
-cursor.execute('SELECT * FROM tab')
-print(cursor.fetchall())
-'''
 
-db.close()
+if __name__ == '__main__':
+    create_table()
+
