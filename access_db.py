496bc7335520a4665004a5da9d4664c8a75dc79b
kur <rhdnfka94@gmail.com>
2019-03-30 09:19:10 +0900

    Add num_rows

--- access_db.py
+++ access_db.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+# TODO: extract with, and get db as arg.
 def img_pathseq(get_sorted=True, incremental=True):
     with sqlite3.connect('szmc.db') as db:
         db.execute('PRAGMA foreign_keys = ON;')
@@ -16,9 +17,23 @@ def img_pathseq(get_sorted=True, incremental=True):
 
         return cur.fetchall()
 
+def num_rows(table):
+    with sqlite3.connect('szmc.db') as db:
+        db.execute('PRAGMA foreign_keys = ON;')
+        cur = db.cursor()
+        cur.execute('SELECT count(*) FROM %s' % table)
+        n_rows = cur.fetchone()[0]
+        return n_rows
+
+def update_work_state(order, new_id):
+    pass
+
 if __name__ == '__main__':
+    '''
     rows = img_pathseq()
     print(*rows, sep='\n')
     print('--------------------------------------------------')
     rows = img_pathseq(incremental=False)
     print(*rows, sep='\n')
+    '''
+    print(num_rows('work_state'))
