51f8fa9a11b05fc6e07b74412ada1c4c93a40680
kur <rhdnfka94@gmail.com>
2019-04-15 16:39:40 +0900

    Add scropt: imgcut->json->flist

---
+++ make_flist_tmp0415.py
@@ -0,0 +1,19 @@
+import json
+from random import shuffle
+import fp
+import sys
+
+src_json = sys.argv[1] # json created from image_cutter #'./190414cropping.json' 
+dst_flist= sys.argv[2] # dataset flist file #'HN_190414_valid.flist' 
+
+with open(src_json) as f:
+    dic = json.load(f)
+
+with open(dst_flist,'w') as f:
+    lines = fp.pipe(
+        fp.cmap(lambda tup: tup[0]),
+        fp.clmap(lambda s: s + '\n'),
+    )(dic.items())
+
+    shuffle(lines)
+    f.writelines(lines)
