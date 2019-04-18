8d6fe75c2e1a7ce62087ca921dfdc6e1ab40385a
kur <rhdnfka94@gmail.com>
2019-04-18 16:39:41 +0900

    Add some functions to fp.py

--- fp.py
+++ fp.py
@@ -1,5 +1,6 @@
 import funcy as F
 import functools
+import itertools
 
 def wrap(f, wrapper):
     @functools.wraps(f)
@@ -14,12 +15,28 @@ def unzip(zipped):
 def foreach(f, seq):
     for elem in seq:
         f(elem)
+permutations = itertools.permutations
+lpermutations = lambda seq: list(itertools.permutations(seq))
 
+reduce = functools.reduce
+#creduce = F.curry(functools.reduce) # cannot curry reduce,,,??? it's impl issue!!
+plus = lambda a,b: a+b
+sub1 = lambda x: x - 1
 
 first = F.first
+remove = F.remove
+lremove = F.lremove
+cremove = F.curry(F.remove)
+clremove = F.curry(F.lremove)
+
 partial = F.partial
 curry = F.curry
 tap = F.tap
+#ctap = F.curry(F.tap) #TODO
+
+chunks = F.chunks
+cchunks = F.curry(F.chunks)
+clchunks = F.curry(F.lchunks)
 
 flatten = F.flatten
 lflatten = F.lflatten
@@ -36,8 +53,12 @@ lmap = F.lmap
 cmap = F.curry(F.map)
 clmap = F.curry(F.lmap)
 
-repeat = F.repeat
+cmapcat = F.curry(F.mapcat)
 
+repeat = F.repeat
+pairwise = F.pairwise
+butlast = F.butlast
+lbutlast = lambda seq: list(F.butlast(seq))
 zipdict = F.zipdict
 cmerge_with = F.curry(F.merge_with)
 merge = F.merge
@@ -47,4 +68,3 @@ into = lambda f: lambda xs: F.walk(f, xs)
 ginto = lambda f: lambda xs: map(f, xs)
 linto = lambda f: lambda xs: list(map(f, xs))
 flip = lambda f: lambda *args,**kargs: f(*reversed(args),**kargs)
-
