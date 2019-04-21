526e7b1266597c8270a4707a0867ab20e69f21a9
kur <rhdnfka94@gmail.com>
2019-04-21 16:30:28 +0900

    get dataset dictionary

--- mk_snet_dset.py
+++ mk_snet_dset.py
@@ -1,6 +1,9 @@
+import cv2
 import sys
 from pathlib import Path
 import pandas as pd
+from futils import human_sorted, file_pathseq
+from imutils import categorize
 
 import fp
 
@@ -50,7 +53,6 @@ idxs_table = { # rdt = tRain / Dev / Test
     100: fp.zipdict(keys, fp.map(sample, full_idxs_list,[1/2]*3)),
      50: fp.zipdict(keys, fp.map(sample, full_idxs_list,[1/4]*3)),
 }
-##################################################################
 
 '''
 print('-------200-------')
@@ -65,6 +67,37 @@ for x in idxs_table[100].items(): print(x)
 print('-------50--------')
 for x in idxs_table[50].items(): print(x)
 '''
+###################### Save index list ###########################
+##################################################################
+
+##################################################################
+img_paths = human_sorted(file_pathseq('./snet_data/image/'))
+rbk_paths = score_paths #(from scores.xlsx)
+wk_paths  = human_sorted(file_pathseq('./snet_data/clean_wk/'))
+
+_,rbk_omap= categorize(cv2.imread('./snet_data/rbk_sample.png'))
+_,wk_omap = categorize(cv2.imread('./snet_data/wk_sample.png'))
+
+def dataset(origin_map, rdt_idxs, img_paths, label_paths):
+    return {
+        'origin_map': origin_map,
+
+        'train_imgs': fp.lli_nths(rdt_idxs['train'], img_paths),
+        'valid_imgs': fp.lli_nths(rdt_idxs['valid'], img_paths),
+        'test_imgs':  fp.lli_nths(rdt_idxs[ 'test'], img_paths),
+
+        'train_masks':fp.lli_nths(rdt_idxs['train'], label_paths),
+        'valid_masks':fp.lli_nths(rdt_idxs['valid'], label_paths),
+        'test_masks': fp.lli_nths(rdt_idxs[ 'test'], label_paths),
+    }
+
+dset = dataset(rbk_omap, idxs_table[50], img_paths, rbk_paths)
+for x in dset.items():
+    print(x)
+
+# org_map, idxs -> dset_dict
+
+##################################################################
 
 # img_paths
 # rbk_paths
